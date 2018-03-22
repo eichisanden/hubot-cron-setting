6911c548be0ef199321c9ccb21ec30612d53b562
eichisanden <eichisanden@gmail.com>
2018-03-22 13:14:30 +0900

    add new cron

--- src/setting.js
+++ src/setting.js
@@ -18,6 +18,8 @@ const path = require('path');
 const TextMessage = require('hubot').TextMessage;
 const User = require('hubot').User;
 
+process.env.EXPRESS_STATIC = path.join(__dirname, '..', 'public');
+
 module.exports = (robot) => {
   robot.router.set('views', path.join(__dirname, '..', 'views'));
   robot.router.set('view engine', 'pug');
@@ -26,12 +28,17 @@ module.exports = (robot) => {
     console.log(data);
     res.render('index', { title: 'hubot-setting', data: JSON.stringify(data) });
   });
-  robot.router.get("/save", (req, res) => {
+  robot.router.get("/new-cron", (req, res) => {
+    res.render('new', { title: 'hubot-setting' });
+  });
+  robot.router.post("/new-cron", (req, res) => {
     const dummyId = 999;
     const user = new User(dummyId);
     user.name = 'eichisanden';
-    user.room = 'town-square';
-    const msg = new TextMessage(user, robot.name + " new job 0 9 * * 1-5 \"Good morning everyone!\"", dummyId);
+    user.room = req.body.room;
+    const pattern = req.body.pattern;
+    const message = req.body.message;
+    const msg = new TextMessage(user, `${robot.name} new job ${pattern} "${message}"`, dummyId);
     robot.receive(msg);
     res.render('index', { title: 'hubot-setting', data: msg });
   });
